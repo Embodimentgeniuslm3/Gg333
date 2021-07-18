a77b801ed0a340ab5ef1fbb8ed7f2235e936fa33
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-07-18 19:31:27 +0000

    Initialize feature/duckling_20210625

---
+++ tests/test_query_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_query_cache
+----------------------------------
+
+Tests the generated query cache has to correct key value types.
+
+"""
+# pylint: disable=locally-disabled,redefined-outer-name
+from mindmeld.core import ProcessedQuery
+from mindmeld.query_cache import QueryCache
+
+
+def test_query_cache_has_the_correct_format(kwik_e_mart_app_path):
+    cache = QueryCache(kwik_e_mart_app_path)
+    key = cache.get_key("store_info", "help", "User manual")
+    row_id = cache.key_to_row_id(key)
+    assert row_id is not None
+    pq = cache.get(row_id)
+    assert type(pq) == ProcessedQuery
+    assert pq.domain == "store_info"
+    assert pq.intent == "help"
